c9e64ea3dc3f14bb74cb024dcbfdcafcd5b16513
abolfazl <abolfazl.948233@gmail.com>
2025-02-19 21:41:35 +0330

    b

--- src/components/SiteLoader.jsx
+++ src/components/SiteLoader.jsx
@@ -3,7 +3,7 @@ import BeatLoader from 'react-spinners/BeatLoader'
 export default function SiteLoader() {
     return (
         <div className='site_loader fixed flex items-center justify-center inset-0 z-[999] bg-Primary'>
-            <img src="/img/svg/site-logo-loader.svg" alt="" />
+            <img src="../public/img/svg/site-logo-loader.svg" alt="" />
             <div className='absolute bottom-10'>
                 <BeatLoader color='#fff' />
             </div>
