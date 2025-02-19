2eb058a66c9762fe43f8035823fbc304473acac3
abolfazl <abolfazl.948233@gmail.com>
2025-02-19 22:58:16 +0330

    w

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -7,7 +7,7 @@ export default function Contact() {
     return (
         <>
             <Header />
-            <HeaderPages title="با ترخینه در تماس باشید." bgimg="bg-[url(../public/img/png/contact-header.jpeg)]" />
+            <HeaderPages title="با ترخینه در تماس باشید." bgimg="bg-[url(/img/png/contact-header.jpeg)]" />
             <main className='container my-12 flex flex-col gap-y-6'>
                 <BranchBoxPage title="شعبه اکباتان" src="/img/png/branch-img-2.jpeg" address="آدرس: شهرک اکباتان، فاز ۳، مجتمع تجاری کوروش، طبقه سوم" phone="۰۲۱-۵۴۸۹۱۲۵۰-۵۱" />
                 <BranchBoxPage title="شعبه چالوس" src="/img/png/branch-img-4.jpeg" address="آدرس: چالوس، خیابان ۱۷ شهریور، جنب داروخانه دکتر میلانی" phone="۰۲۱-۵۴۸۹۱۲۵۲-۵۳" />
