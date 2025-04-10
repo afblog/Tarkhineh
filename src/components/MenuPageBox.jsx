7b59bc8ccd4d8e2ae139c36699cf08038b4db4b4
abolfazl <abolfazl.948233@gmail.com>
2025-04-10 18:06:10 +0330

    edite

--- src/components/MenuPageBox.jsx
+++ src/components/MenuPageBox.jsx
@@ -101,7 +101,7 @@ export default function MenuPageBox({ title, src, price, description, discount, 
                                     <button onClick={() => {
                                         setIsAddShoppingCart(true)
                                         addToCart(product)
-
+                                        console.log(discount, percent);  
                                     }} className='py-1.5 px-1 md:px-2 xl:px-14 bg-Primary text-white text-[10px] md:text-xs lg:text-base font-EstedadRegular lg:font-EstedadMedium rounded-sm cursor-pointer hover:bg-Shade-1 transition-all text-nowrap'>افزودن به سبد خرید</button>
                                 )
                             }
