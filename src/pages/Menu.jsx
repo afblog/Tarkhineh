9637e2382629fdc8f51ed7e00539f3b5277b42a3
abolfazl <abolfazl.948233@gmail.com>
2025-04-10 18:01:11 +0330

    edite menu

--- src/pages/Menu.jsx
+++ src/pages/Menu.jsx
@@ -124,7 +124,7 @@ export default function Menu() {
 
                       mainCourse.nonIranianFoods.map((item) => (
                         <div key={item.id}>
-                          <MenuPageBox {...item} />
+                          <MenuPageBox {...item} product={item}/>
                         </div>
                       ))
                     }
@@ -137,7 +137,7 @@ export default function Menu() {
                     {
                       mainCourse.pizzas.map((item) => (
                         <div key={item.id}>
-                          <MenuPageBox {...item} />
+                          <MenuPageBox {...item} product={item}/>
                         </div>
                       ))
                     }
@@ -150,7 +150,7 @@ export default function Menu() {
                     {
                       mainCourse.sandwiches.map((item) => (
                         <div key={item.id}>
-                          <MenuPageBox {...item} />
+                          <MenuPageBox {...item} product={item}/>
                         </div>
                       ))
                     }
@@ -180,7 +180,7 @@ export default function Menu() {
                     {
                       appetizer.iranianAppetizer.map((item) => (
                         <div key={item.id}>
-                          <MenuPageBox {...item} />
+                          <MenuPageBox {...item} product={item}/>
                         </div>
                       ))
                     }
@@ -192,7 +192,7 @@ export default function Menu() {
                     {
                       appetizer.nonIranianAppetizer.map((item) => (
                         <div key={item.id}>
-                          <MenuPageBox {...item} />
+                          <MenuPageBox {...item} product={item}/>
                         </div>
                       ))
                     }
@@ -223,7 +223,7 @@ export default function Menu() {
                     {
                       dessert.iranianDessert.map((item) => (
                         <div key={item.id}>
-                          <MenuPageBox {...item} />
+                          <MenuPageBox {...item} product={item}/>
                         </div>
                       ))
                     }
@@ -235,7 +235,7 @@ export default function Menu() {
                     {
                       dessert.nonIranianDessert.map((item) => (
                         <div key={item.id}>
-                          <MenuPageBox {...item} />
+                          <MenuPageBox {...item} product={item}/>
                         </div>
                       ))
                     }
@@ -265,7 +265,7 @@ export default function Menu() {
                     {
                       drink.iranianDrink.map((item) => (
                         <div key={item.id}>
-                          <MenuPageBox {...item} />
+                          <MenuPageBox {...item} product={item}/>
                         </div>
                       ))
                     }
